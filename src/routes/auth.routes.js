d6de1dc45e11ef65843e3f7bcfbc7575b02cabea
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2026-01-14 23:44:58 +0530

    cookie-parser and jwt token handling in registration route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const UserModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -10,16 +11,34 @@ const router = express.Router();
 //login route //post
 //user profile route //get
 
-router.post('/register',  async (req,res)=>{
-    const {username,password} = req.body;
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    const existingUser = await UserModel.findOne({ username });
+
+    if (existingUser) {
+        return res.status(409).json({
+            message: "username already exists"
+        })
+
+    }
 
     const user = await UserModel.create({
-        username,password
-    }) 
+        username, password
+    })
 
+    const Token = jwt.sign({
+        userid: user._id //._id is mongoose default id
+    },process.env.jwt_secret,{
+        expiresIn: '1h'
+    })
+
+    res.cookie('token',Token);
+
+    
 
     res.status(201).json({
-        message : "user created successfully",
+        message: "user created successfully",
         user
     })
 
