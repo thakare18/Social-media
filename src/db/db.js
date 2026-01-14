88f8e38b3c1b97f5255c43eb6d12b8f9658ca452
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2026-01-14 19:49:35 +0530

    server setup and database connection

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect("process.env.MONGODB_URI")
+.then(() => {
+    console.log("Connected to MongoDB");
+}
+).catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+}
+);
+
+module.exports = mongoose;
