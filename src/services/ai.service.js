1f62148d6a70d556d9d948871b6a38139da3f257
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2026-01-20 14:49:49 +0530

    improved caption generation - system intruction added

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -2,30 +2,30 @@ const { GoogleGenAI } = require("@google/genai");
 
 const ai = new GoogleGenAI({});
 
-async function generateCaption(base64ImageFile){
+async function generateCaption(base64ImageFile) {
     const contents = [
         {
-            inlineData : {
+            inlineData: {
                 mimeType: "image/jpeg",
-                data : base64ImageFile,
+                data: base64ImageFile,
             },
         },
-        {text: "Caption this image."},
+        { text: "Caption this image." },
     ];
 
 
-const responce = await ai.models.generateContent({
-    model : "gemini-2.5-flash",
-    contents : contents,
-    config: {
-        systemInstruction: `You are a expert for generrating a caption for the images. 
+    const responce = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: contents,
+        config: {
+            systemInstruction: `You are a expert for generrating a caption for the images. 
         You must generate a single caption for the given images.
          Your caption should be short and concise.
          You can use hashtags and emojis for making a more attractive caption.`
-    }
-    
-});
-return responce.text;
+        }
+
+    });
+    return responce.text;
 }
 
 module.export = generateCaption;
